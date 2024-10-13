e2a7a011a6685385ce7d663679bf80780c623559
SragZhranOBS <srag.zhran@orange.com>
2024-10-13 12:44:16 +0300

    webhook test

--- Front-Backend -- App/backend/app.py
+++ Front-Backend -- App/backend/app.py
@@ -20,7 +20,7 @@ def get_db_connection():
 
 @app.route('/')
 def home():
-    return "Welcome to the Hotel "
+    return "Welcome to the DEV-IT-OPS Hotel "
 
 
 @app.route('/hotels', methods=['GET'])
